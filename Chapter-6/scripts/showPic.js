18faa1a0e3b73f3784c3bcc77276c3daf9832790
lincoco <962894047@qq.com>
2021-05-18 08:17:32 +0800

    html 与javascript分离:onclick事件绑定挪到js文件中

--- Chapter-6/scripts/showPic.js
+++ Chapter-6/scripts/showPic.js
@@ -1,4 +1,4 @@
-window.onload=countBodyChildren;
+window.onload=prepareGallery;
 
 function showPic(whichpic){
     var source=whichpic.getAttribute("href");
@@ -8,6 +8,20 @@ function showPic(whichpic){
     var text=whichpic.getAttribute("title");
     var description=document.getElementById("description");
     description.firstChild.nodeValue=text;
+}
+
+function prepareGallery(){
+    if (!document.getElementsByTagName || !document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var gallery=document.getElementById("imagegallery");
+    var links=gallery.getElementsByTagName("a");
+    for (var i=0;i<links.length;i++){
+        links[i].onclick=function(){
+            showPic(this);
+            return false;
+        };
+    }
+
 }
 
 function countBodyChildren(){
