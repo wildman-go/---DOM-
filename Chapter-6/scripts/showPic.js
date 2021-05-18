d4bcfd943fce0eda021416ae0173ac6f7137f620
lincoco <962894047@qq.com>
2021-05-18 08:40:59 +0800

    增加更多元素名称和类型的检查
    实际工作中，根据需要决定是否真的需要这些检查；一般在html不可控的时候，需要加
    
    Signed-off-by: lincoco <962894047@qq.com>

--- Chapter-6/scripts/showPic.js
+++ Chapter-6/scripts/showPic.js
@@ -4,12 +4,15 @@ function showPic(whichpic){
     if (!document.getElementById("placeholder")) return false;
     var source=whichpic.getAttribute("href");
     var placeholder=document.getElementById("placeholder");
+    if (placeholder.nodeName != "IMG") return false;
     placeholder.setAttribute("src", source);
 
     if (document.getElementById("description")){
-        var text=whichpic.getAttribute("title");
+        var text=whichpic.getAttribute("title") ? whichpic.getAttribute("title"):"";
         var description=document.getElementById("description");
-        description.firstChild.nodeValue=text;
+        if (description.firstChild.nodeType == 3){
+            description.firstChild.nodeValue=text;
+        }
     }
     return true;
 }
