55a59f888cb6ca3a433a05db440484dfb33c6dd6
lincoco <962894047@qq.com>
2021-05-18 08:30:17 +0800

    检查html中的placeholder和description的元素是否存在

--- Chapter-6/scripts/showPic.js
+++ Chapter-6/scripts/showPic.js
@@ -1,13 +1,17 @@
 addLoadEvent(prepareGallery);
 
 function showPic(whichpic){
+    if (!document.getElementById("placeholder")) return false;
     var source=whichpic.getAttribute("href");
     var placeholder=document.getElementById("placeholder");
     placeholder.setAttribute("src", source);
-    
-    var text=whichpic.getAttribute("title");
-    var description=document.getElementById("description");
-    description.firstChild.nodeValue=text;
+
+    if (document.getElementById("description")){
+        var text=whichpic.getAttribute("title");
+        var description=document.getElementById("description");
+        description.firstChild.nodeValue=text;
+    }
+    return true;
 }
 
 function prepareGallery(){
