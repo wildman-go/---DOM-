f8c841880e0d609559230b2c5f9e80e16f9cd485
lincoco <962894047@qq.com>
2021-05-22 15:13:02 +0800

    抽象：将一个特点的元素以特定的速度移动到一个特定的位置

--- Chapter-10/scripts/positionMessage.js
+++ Chapter-10/scripts/positionMessage.js
@@ -7,42 +7,7 @@ function positionMessage(){
     elem.style.top="100px";
 
     // movement=setTimeout("moveMessage()", 5000);
-    moveMessage();
-
-}
-
-function moveMessage(){
-    if(!document.getElementById) return false;
-    if(!document.getElementById("message")) return false;
-    var elem=document.getElementById("message");
-
-    var xpos=parseInt(elem.style.left);
-    var ypos=parseInt(elem.style.top);
-
-    if(xpos==200 && ypos==200){
-        return true;
-    }
-
-    if(xpos<200){
-        xpos++;
-    }
-
-    if(xpos>200){
-        xpos--;
-    }
-
-    if(ypos<200){
-        ypos++;
-    }
-
-    if(ypos>200){
-        ypos--;
-    }
-
-    elem.style.left=xpos+"px";
-    elem.style.top=ypos+"px";
-
-    movement=setTimeout("moveMessage()",10);
+    moveElement("message",200,200,10);
 
 }
 
