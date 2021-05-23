021af98cf9c477d5068060e764fca8dfe373c340
lincoco <962894047@qq.com>
2021-05-23 16:34:33 +0800

    适配不支持html5的浏览器：关于input的placeholder属性

--- Chapter-12/scripts/global.js
+++ Chapter-12/scripts/global.js
@@ -349,6 +349,36 @@ function focusLabels(){
     }
 }
 
+function resetFields(whichform){
+    if (Modernizer.input.placeholder) return;
+    for (var i=0;i<whichform.elements.length;i++){
+        var element=whichform.elements[i];
+        if(element.type=="submit") continue;
+
+        var check=element.placeholder||element.getAttribute("placeholder"); //鉴于不用的浏览器对未知属性的实现方式有所不同，同时使用两种属性读取方式
+        if (!check) continue;
+        element.onfocus=function(){
+            this.className="";
+            this.value="";
+        }
+        element.onblur=function(){
+            if (this.value==""){
+                this.className="placeholder";
+                this.value=this.placeholder ||this.getAttribute("placeholder");
+            }
+        }
+
+        element.onblur();//在onblur事件定义之后，立即调用了它，以便在必要时应用占位符值。
+    }
+}
+
+function prepareForms(){
+    for (var i=0;i<document.forms.length;i++){
+        var thisform=document.forms[i];
+        resetFields(thisform);
+    }
+}
+
 
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
@@ -359,3 +389,4 @@ addLoadEvent(stripTables);
 addLoadEvent(highlightRows);
 addLoadEvent(displayAbbreviations);
 addLoadEvent(focusLabels);
+addLoadEvent(prepareForms);
