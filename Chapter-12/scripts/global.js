040d5c266217cfdb306bc399829304d561c6197a
lincoco <962894047@qq.com>
2021-05-23 12:42:06 +0800

    单击一个目录项，就只显示相关的部分

--- Chapter-12/scripts/global.js
+++ Chapter-12/scripts/global.js
@@ -162,6 +162,40 @@ function prepareSlideshow(){
 
 }
 
+function showSection(id){
+    var sections=document.getElementsByTagName("section");
+    for (var i=0; i<sections.length; i++){
+        if (sections[i].getAttribute("id") != id){
+            sections[i].style.display="none";
+        }else{
+            sections[i].style.display="block";
+        }
+    }
+}
+
+function prepareInternalnav(){
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    var articles=document.getElementsByTagName("article");
+    if (articles.length ==0) return false;
+    var navs=articles[0].getElementsByTagName("nav");
+    if(navs.length==0) return false;
+
+    var nav=navs[0];
+    var links=nav.getElementsByTagName("a");
+    for (var i=0; i<links.length;i++){
+        var sectionId=links[i].href.split("#")[1];
+        if (!document.getElementById(sectionId)) continue;
+        document.getElementById(sectionId).style.display="none";
+        links[i].destination=sectionId;
+        links[i].onclick=function(){
+            showSection(this.destination);
+            return false;
+        }
+    }
+}
+
 
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
+addLoadEvent(prepareInternalnav);
