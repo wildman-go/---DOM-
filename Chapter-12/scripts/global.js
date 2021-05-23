b4cf44d6c97333180ad1924324559baac0211170
lincoco <962894047@qq.com>
2021-05-23 11:48:59 +0800

    创建动画

--- Chapter-12/scripts/global.js
+++ Chapter-12/scripts/global.js
@@ -53,4 +53,108 @@ function highlightPage(){
 }
 
 
+function moveElement(elementID, final_x, final_y, interval){
+    if(!document.getElementById) return false;
+    if(!document.getElementById(elementID)) return false;
+    var elem=document.getElementById(elementID);
+    if (elem.movement){
+        clearTimeout(elem.movement);
+    }
+
+    //检查元素有无left、top的样式熟悉
+    if(!elem.style.left){
+        elem.style.left="0px";
+    }
+    if(!elem.style.top){
+        elem.style.top="0px";
+    }
+
+    var xpos=parseInt(elem.style.left);
+    var ypos=parseInt(elem.style.top);
+    var dist;
+
+    if(xpos==final_x && ypos==final_y){
+        return true;
+    }
+
+    if(xpos<final_x){
+        dist=Math.ceil((final_x-xpos)/10);
+        xpos=xpos+dist;
+    }
+
+    if(xpos>final_x){
+        dist=Math.ceil((xpos-final_x)/10);
+        xpos=xpos-dist;
+    }
+
+    if(ypos<final_y){
+        dist=Math.ceil((final_y-ypos)/10);
+        ypos=ypos+dist;
+    }
+
+    if(ypos>final_y){
+        dist=Math.ceil((ypos-final_y)/10);
+        ypos=ypos-dist;
+    }
+
+    elem.style.left=xpos+"px";
+    elem.style.top=ypos+"px";
+
+    repeat="moveElement('"+elementID+"',"+ final_x+","+final_y+","+ interval+")";
+
+    elem.movement=setTimeout(repeat,interval);
+
+}
+
+
+function prepareSlideshow(){
+    if(!document.getElementById) return false;
+    if(!document.getElementsByTagName) return false;
+    if(!document.getElementById("intro")) return false;
+    //获取article中的intro段落
+    var intro=document.getElementById("intro");
+
+    // 创建放preview图片的div容器
+    var slideshow=document.createElement("div");
+    slideshow.setAttribute("id", "slideshow");
+    
+    // 创建preview 的img
+    var preview=document.createElement("img");
+    preview.setAttribute("src", "images/slideshow.gif");
+    preview.setAttribute("alt", "a glimpse of what awaits you");
+    preview.setAttribute("id", "preview");
+
+    // 将preview添加进slideshow容器；将slideshow放到intro后面
+    slideshow.appendChild(preview);
+    insertAfter(slideshow, intro);
+
+    //遍历intro中的链接
+    var links=intro.getElementsByTagName("a");
+    var destination;
+    for (var i=0;i<links.length;i++){
+        links[i].onmouseover=function(){
+            destination=this.getAttribute("href");
+            if (destination.indexOf("index.html")!=-1){
+                moveElement("preview",0,0,5);
+            }
+            if (destination.indexOf("about.html")!=-1){
+                moveElement("preview",-150,0,5);
+            }
+            if (destination.indexOf("photos.html")!=-1){
+                moveElement("preview",-300,0,5);
+            }
+            if (destination.indexOf("live.html")!=-1){
+                moveElement("preview",-450,0,5);
+            }
+            if (destination.indexOf("contact.html")!=-1){
+                moveElement("preview", -600, 0, 5);
+            }
+        }
+    }
+
+
+}
+
+
 addLoadEvent(highlightPage);
+addLoadEvent(prepareSlideshow);
