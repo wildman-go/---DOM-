b209300701756def2bae5e0df946350f4bd73ffa
lincoco <962894047@qq.com>
2021-06-06 23:05:10 +0800

    vue: 用v-model实现数据的双向绑定

--- vue-learn/vue-demo/src/App.vue
+++ vue-learn/vue-demo/src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-      {{message}}
-      <div v-bind:id="message"></div>
-          <todo-list>
-              <todo-item @delete="handleDelete" v-for="item in list" v-bind:key="item" :title="item.title" :del="item.del">
-                  <!-- 2.6的语法 -->
-                  <template v-slot:pre-icon="{value}">
-                      <span>前置图标, {{value}}</span>
-                  
-                  </template>
-                  <!-- 2.5的语法 -->
-                  <!-- <span slot="suf-icon">后置图标</span> -->
+    <input v-model="message">
+    {{message}}
+    <div v-bind:id="message"></div>
+        <todo-list>
+            <todo-item @delete="handleDelete" v-for="item in list" v-bind:key="item" :title="item.title" :del="item.del">
+                <!-- 2.6的语法 -->
+                <template v-slot:pre-icon="{value}">
+                    <span>前置图标, {{value}}</span>
+                
+                </template>
+                <!-- 2.5的语法 -->
+                <!-- <span slot="suf-icon">后置图标</span> -->
 
-              </todo-item>
-          </todo-list>
+            </todo-item>
+        </todo-list>
   </div>
 </template>
 
